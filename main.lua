06609c5ce4a5ec216face4f71f1bb5cd004d66cd
TheEt1234 <TheEt1234@users.noreply.github.com>
2024-10-06 17:58:22 +0200

    oops

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ libox = {
     safe = {},
     supply_additional_environment = function(...) return ... end, -- for other mods
     default_hook_time = 20,
-    disabled = {}
+    disabled = false
 }
 
 
